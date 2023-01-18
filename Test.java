036872c5d047d4070aee4ff0156d5f9423a306c0
Ebug0 <112724165+Ebug0@users.noreply.github.com>
2023-01-18 17:21:12 -0600

    Added stuff

--- Test.java
+++ Test.java
@@ -1,12 +1,19 @@
+import java.util.*;
+
 public class Test
 {
 public static void main(String[] args)
     {
-
-    System.out.println("Hello World");
-
-    
-
+    int n = 8;
+    if (n == 0){
+        System.out.println("Then input is 0");
+    }
+    else if (n % 2 == 0){
+        System.out.println("The input is even");
+    }
+    if (n >= 0 & n < 10){
+        System.out.println("The input has a single digit.");
+    }
 
     }
 }
