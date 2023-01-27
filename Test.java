db45837b61ebf03e527505a5ae2da28d243fda40
Ebug0 <ethanlivermore@hotmail.com>
2023-01-27 14:04:53 -0600

    changes

--- Test.java
+++ Test.java
@@ -3,18 +3,10 @@ public class Test
 public static void main(String[] args)
     {
 
-;
-    String one = "his";
-    String two = "cycle";
-    
-
-    System.out.println(one.compareTo(two));
-     if (one.compareTo(two) < 0)
-        System.out.println(two);
-    
-    else{
-        System.out.println(one);
+    boolean b = false;
+    do {
+        System.out.println("hi");
     }
- 
+        while (b != b)
     }
 }
